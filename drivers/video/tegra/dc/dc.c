df9974e908f8829e63287692adba85cc75c7d173
CTCaer <ctcaer@gmail.com>
2021-05-24 13:07:33 +0000

    dc: add support for DSI DT based EDID

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -82,6 +82,7 @@ EXPORT_TRACEPOINT_SYMBOL(display_readl);
 #include "nvhost_syncpt.h"	/* Preset and flush vblank_syncpt*/
 #include "dpaux.h"
 #include "lvds.h"
+#include "dsi.h"
 #include "dc_common.h"
 
 #include "edid.h"
@@ -1532,14 +1533,6 @@ static int dbg_edid_show(struct seq_file *s, void *unused)
 
 	if (WARN_ON(!dc || !dc->out))
 		return -EINVAL;
-
-	if (dc->out->type == TEGRA_DC_OUT_DSI ||
-		dc->out->type == TEGRA_DC_OUT_FAKE_DSIA ||
-		dc->out->type == TEGRA_DC_OUT_FAKE_DSIB ||
-		dc->out->type == TEGRA_DC_OUT_FAKE_DSI_GANGED) {
-		seq_puts(s, "No EDID\n");
-		return 0;
-	}
 
 	if (WARN_ON(!dc->edid))
 		return -EINVAL;
@@ -6703,12 +6696,20 @@ static int tegra_dc_probe(struct platform_device *ndev)
 	dc->boot_topology.valid = true;
 	dc->current_topology = dc->boot_topology;
 
-	if ((dc->pdata->flags & TEGRA_DC_FLAG_ENABLED) &&
-		dc->out->type == TEGRA_DC_OUT_LVDS) {
-		struct fb_monspecs specs;
-		struct tegra_dc_lvds_data *lvds = tegra_dc_get_outdata(dc);
-		if (!tegra_edid_get_monspecs(lvds->edid, &specs))
-			tegra_dc_set_fb_mode(dc, specs.modedb, false);
+	if (dc->pdata->flags & TEGRA_DC_FLAG_ENABLED) {
+		if (dc->out->type == TEGRA_DC_OUT_LVDS) {
+			struct fb_monspecs specs;
+			struct tegra_dc_lvds_data *lvds = tegra_dc_get_outdata(dc);
+			if (!tegra_edid_get_monspecs(lvds->edid, &specs))
+				tegra_dc_set_fb_mode(dc, specs.modedb, false);
+		} else if (dc->out->type == TEGRA_DC_OUT_DSI ||
+			  dc->out->type == TEGRA_DC_OUT_FAKE_DSIA ||
+			  dc->out->type == TEGRA_DC_OUT_FAKE_DSIB ||
+			  dc->out->type == TEGRA_DC_OUT_FAKE_DSI_GANGED) {
+			struct fb_monspecs specs;
+			struct tegra_dc_dsi_data *dsi = tegra_dc_get_outdata(dc);
+			tegra_edid_get_monspecs(dsi->hpd_data.edid, &specs);
+		}
 	}
 
 #ifndef CONFIG_TEGRA_ISOMGR
